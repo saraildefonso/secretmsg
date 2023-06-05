161e6647b549055c3181cd9581f45b3b62bf0624
Sara Ildefonso <117453863+saraildefonso@users.noreply.github.com>
2023-06-05 15:06:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const { hash } = window.location;
+
+const msg = atob(hash.replace("#", ""));
+
+if (msg) {
+  document.querySelector("#msg-form").classList.add("hide");
+  document.querySelector("#msg-show").classList.remove("hide");
+  document.querySelector("h1").innerHTML = msg;
+}
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  document.querySelector("#msg-form").classList.add("hide");
+  document.querySelector("#link-form").classList.remove("hide");
+
+  const input = document.querySelector("#msg-input");
+  const encrypted = btoa(input.value);
+
+  const linkInput = document.querySelector("#link-input");
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
+});
